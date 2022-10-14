d7c903bb5f2181908d51aa8a020ae553372a3a81
Mazstar04 <mazeedahhamzat@gmail.com>
2022-10-14 11:17:52 +0100

    init

---
+++ IllegalGoods/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(IllegalGoods("101"));
+
+static int IllegalGoods(string s){
+    int min = 0;
+    while(s.IndexOf("1") != -1)
+    {
+        if(s[0] == '1' )
+        {
+            s = s.Substring(1);
+            min++;
+        }
+        else if(s[s.Length -1] == '1')
+        {
+            s = s.Substring(0, s.Length-1);
+            min++;
+        }
+        else if(s.Length > 1 && s[1] == '1')
+        {
+            s = s.Substring(1);
+            min++;
+        }
+        else if(s.Length > 1 && s[s.Length-2] == '1')
+        {
+            s = s.Substring(0, s.Length-1);
+            min++;
+        }
+        else{
+            s= s.Substring(0, s.IndexOf('1')) + s.Substring(s.IndexOf('1')+1);
+            min+=2;
+        }
+    }
+    return min;
+}
---
+++ LargestRectangle/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(LargestRectangle(new int[] {1,2,3,4,5}));
+
+static long LargestRectangle(int[] h)
+{
+    long max = 0;
+    for(int i = 0; i< h.Length; i++)
+    {
+        long con = 1;
+        for(int j = i+1; j<h.Length; j++)
+        {
+            if(h[i] <= h[j]) {
+                con++;
+            }
+            else break;
+        }
+
+        for(int j = i-1; j>=0; j--)
+        {
+            if(h[i] <= h[j]) {
+                con++;
+            }
+            else break;
+        }
+        long currentMax = h[i]*con;
+
+        if(currentMax> max) max= currentMax;
+
+    }
+    return max;
+}
